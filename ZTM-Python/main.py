c583702d30596e90e271e93b68f254dac7368ee2
Ryan <98045919+Woodedcode@users.noreply.github.com>
2025-03-31 16:03:46 -0500

    Update main.py

--- ZTM-Python/main.py
+++ ZTM-Python/main.py
@@ -2,3 +2,20 @@ print("Ryan Woods")
 
 name = input("What is your name?")
 print(' hellllloooooo ' + name)
+
+
+#Fundamental Data Types
+int
+float
+bool
+str
+list 
+tuple
+set
+dict
+
+#Classes -> custom types
+
+#Specialized Data Types
+
+None
