3b91b7c47c981d24f5e437088d729b1c2205fb73
Ryan <98045919+Woodedcode@users.noreply.github.com>
2025-04-04 22:02:02 -0500

    Update main.py

--- ZTM-Python/main.py
+++ ZTM-Python/main.py
@@ -119,6 +119,24 @@ print(full_name)
 
 
 
+############ STRINGS CONCATENATION ############
+print('Hello' + 'Ryan')
+
+
+
+
+############ TYPE CONVERSION ############
+print(str(100))
+print(type(str(100)))
+print(type(int(str(100))))
+
+
+a = 100
+b = int(a)
+c = type(b)
+print(c)
+
+
 
 
 
